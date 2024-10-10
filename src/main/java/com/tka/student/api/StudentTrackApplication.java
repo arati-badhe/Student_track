ba18744c58adc03b47f85cc87231ed8da6aa9cda
badhe arati <badhea02@gmail.com>
2024-10-10 11:46:21 +0530

    first commit

---
+++ src/main/java/com/tka/student/api/StudentTrackApplication.java
@@ -0,0 +1,13 @@
+package com.tka.student.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrackApplication.class, args);
+	}
+
+}
